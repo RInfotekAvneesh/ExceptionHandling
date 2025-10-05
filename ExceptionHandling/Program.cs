d97e67570b7a0f4318ddc0fa8f48ab79b02bdd99
RInfotekAvneesh <avneeshj832@gmail.com>
2025-10-05 12:02:51 +0530

    Add project files.

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter first Number");
+        int num1=int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter second NUmber");
+        int num2=int.Parse(Console.ReadLine());
+        try
+        {
+            int result = num1 / num2;
+            Console.WriteLine("Division Result is:" + result);
+
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("You can not divide a number by Zero");
+  Console.WriteLine(ex.Message);
+        }
+        
+        Console.WriteLine("Remaining Program-1");
+        Console.WriteLine("Remaining Program-2");
+        Console.WriteLine("Remaining Program-3");
+        Console.WriteLine("Remaining Program-4");
+
+
+    }
+}
